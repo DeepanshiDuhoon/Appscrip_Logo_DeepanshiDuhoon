db982023a3fc02f79e0ed28b5fed2e09f7b24477
DeepanshiDuhoon <duhoon238@gmail.com>
2025-11-02 21:44:50 +0530

    first

--- asscrip-app/src/components/Header/index.js
+++ asscrip-app/src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React, { startTransition } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import { FaSearch, FaHeart, FaShoppingBag, FaUser } from "react-icons/fa";
